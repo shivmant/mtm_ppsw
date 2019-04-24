773e5273b94e00f6cd5df226ed23ffe767a8f5e4
Pawel Woniak <shivmant1@gmail.com>
2019-04-24 09:58:31 +0200

    test

--- current/main.c
+++ current/main.c
@@ -38,3 +38,4 @@ int main()
 	}
 	return 1;
 }
+//git test
--- current/main.d
+++ current/main.d
@@ -1,4 +1,4 @@
 .\main.o: main.c
-.\main.o: D:\programy\keil\ARM\Inc\Philips\LPC21xx.H
+.\main.o: C:\Keil\ARM\Inc\Philips\LPC21xx.H
 .\main.o: led.h
 .\main.o: keyboard.h
